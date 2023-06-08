804d8653e841a07d51de5c0e39d6b2459efb61ec
Daniel-105 <danielpinto105@gmail.com>
2023-06-08 15:55:04 +0100

    cleaned up the code and formated the code.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
 Console.WriteLine("Welcome to the game!");
 Console.WriteLine("Please enter the max number of the range:");
 
-double maxNumber = Convert.ToInt32(Console.ReadLine()); // 10
+double maxNumber = Convert.ToInt32(Console.ReadLine());
 double highestSoFar = maxNumber;
 double lowestSoFar = 1;
 
@@ -17,10 +17,6 @@ string userResponse = Console.ReadLine();
 
 while (userResponse != "yes") 
 {
-//   if (highestSoFar - lowestSoFar == 2) {
-//     Console.WriteLine("Your number is " + lowestSoFar++);
-//     userResponse = Console.ReadLine();
-//   } 
     if (userResponse == "higher") 
     {
       if (highestSoFar - lowestSoFar == 2) 
@@ -28,26 +24,28 @@ while (userResponse != "yes")
         Console.WriteLine("Your number is " + lowestSoFar++);
         userResponse = Console.ReadLine();
       } 
-      else {
-      lowestSoFar = machineGuess; // 5
-      machineGuess = Math.Round(machineGuess + machineGuess/2); // 8
-      Console.WriteLine("Your number is " + machineGuess);
-      userResponse = Console.ReadLine();
+      else 
+      {
+        lowestSoFar = machineGuess; 
+        machineGuess = Math.Round(machineGuess + machineGuess/2); 
+        Console.WriteLine("Your number is " + machineGuess);
+        userResponse = Console.ReadLine();
       }
     
   }
   else if (userResponse == "lower") {
     if (highestSoFar - lowestSoFar == 1) 
-      {
-        Console.WriteLine("Your number is " + lowestSoFar);
-        userResponse = Console.ReadLine();
-      } 
-      else {
-    highestSoFar = machineGuess; // (8)
-    machineGuess = Math.Round(machineGuess - (machineGuess - lowestSoFar) / 2); // 6,5 -> 7
-    Console.WriteLine("Your number is " + machineGuess);
-    userResponse = Console.ReadLine();
-      }
+    {
+      Console.WriteLine("Your number is " + lowestSoFar);
+      userResponse = Console.ReadLine();
+    } 
+    else 
+    {
+      highestSoFar = machineGuess; 
+      machineGuess = Math.Round(machineGuess - (machineGuess - lowestSoFar) / 2); 
+      Console.WriteLine("Your number is " + machineGuess);
+      userResponse = Console.ReadLine();
+    }
   }
 }
 
