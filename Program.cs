ae101272fc941322bddb3cbdeb2f7779b66cb25d
Daniel-105 <danielpinto105@gmail.com>
2023-06-08 14:37:59 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+// Number = 7
+Console.WriteLine("Welcome to the game!");
+Console.WriteLine("Please enter the max number of the range:");
+
+int maxNumber = Convert.ToInt32(Console.ReadLine()); // 10
+int highestSoFar = maxNumber;
+int lowestSoFar = 1;
+
+Console.WriteLine("Max number is " + maxNumber);
+
+int machineGuess = maxNumber / 2;
+Console.WriteLine("Your number is " + machineGuess);
+string userResponse = Console.ReadLine();
+
+
+while (userResponse != "yes") 
+{
+  if (highestSoFar - lowestSoFar == 2) {
+    Console.WriteLine("Your number is " + lowestSoFar++);
+    userResponse = Console.ReadLine();
+  } 
+  else if (userResponse == "higher") {
+    lowestSoFar = machineGuess;
+    machineGuess = Math.Floor(machineGuess + machineGuess/2); // 8
+  }
+{
+  // block of code to be executed if the condition1 is false and condition2 is True
+} 
+}
+
+
+// 
+
+
+
+
+
+
+
